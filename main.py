58e8f72a09b645b43081bc159492c1c965f5b0ff
sRobinson1209 <150472871+sRobinson1209@users.noreply.github.com>
2024-10-17 21:04:25 -0500

    revised the login function to be compliant with new SQL.

--- main.py
+++ main.py
@@ -59,27 +59,39 @@ def redirect_home():
 def login():
     # Output a message if something goes wrong...
     msg = ''
+
     # Check if "username" and "password" POST requests exist (user submitted form)
     if request.method == 'POST' and 'username' in request.form and 'password' in request.form:
         # Create variables for easy access
         username = request.form['username']
         password = request.form['password']
+
+        conn.psycopg2.connect(os.environ.get('DATABASE_URL'),sslmode='require')
+        cursor = conn.cursor
+
         # Check if account exists using MySQL
         #cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
-        cursor.execute('SELECT * FROM accounts WHERE username = %s AND password = %s', (username, password,))
+        try:
+            cursor.execute('SELECT * FROM accounts WHERE username = %s AND password = %s', (username, password,))
         # Fetch one record and return result
-        account = cursor.fetchone()
+            account = cursor.fetchone()
         # If account exists in accounts table in out database
-        if account:
-            # Create session data, we can access this data in other routes
-            session['loggedin'] = True
-            session['id'] = account['id']
-            session['username'] = account['username']
-            # Redirect to home page
-            return redirect(url_for('home'))
-        else:
-            # Account doesnt exist or username/password incorrect
-            msg = 'Incorrect username/password!'
+            if account:
+                # Create session data, we can access this data in other routes
+                session['loggedin'] = True
+                session['id'] = account['id']
+                session['username'] = account['username']
+                # Redirect to home page
+                return redirect(url_for('home'))
+            else:
+                # Account doesnt exist or username/password incorrect
+                msg = 'Incorrect username/password!'
+        except Exception as e:
+            msg = str(e)
+        finally:
+            cursor.close()
+            conn.close()
+            
     # Show the login form with message (if any)
     return render_template('index.html', msg=msg)
 # http://localhost:5000/python/logout - this will be the logout page
