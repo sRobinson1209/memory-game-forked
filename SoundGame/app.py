1914a18d920d8ed397c3995361aaa28e1a08d37d
thepowerKey888 <thepower674@gmail.com>
2024-11-06 22:50:39 -0600

    Added user interface files such as the app.py file, styles.css, javascript file, and html file. Began some functionality such as a start button and mapping MIDI notes to squares.

--- SoundGame/app.py
+++ SoundGame/app.py
@@ -10,150 +10,30 @@ Basically Separting backedn logic from frontent logic
 - back end is the python file that has the game in it
 '''
 
-from flask import Flask, jsonify, request
+from flask import Flask, send_file, jsonify
 from flask_cors import CORS
-import pretty_midi
-import random
-import pygame
 import os
-import time
+
+current_length = 3
+current_key_scale = [60, 62, 64, 65, 67, 69, 71, 72]  
 
 app = Flask(__name__)
 CORS(app)
 
-# Globals and game state initialization
-current_instrument = 'Acoustic Grand Piano'
-current_key = 'c'
-current_key_scale = [60, 62, 64, 65, 67, 69, 71, 72]
-random_seq = []
-current_speed = 1
-current_length = 3
-level = 0
-score = 0
-MIDI_FILE_PATH = 'generated_melody.mid'
+@app.route('/')
+def melody_memory():
+    return send_file('index.html')
 
-#generates a new key
-def generate_new_key():
-
-    global current_key, current_key_scale
-    key_dictionary = {
-        "c": [60, 62, 64, 65, 67, 69, 71, 72],
-        "d": [62, 64, 66, 67, 69, 71, 73, 74],
-        "e": [64, 66, 68, 69, 71, 73, 75, 76],
-        "f": [65, 67, 69, 70, 72, 74, 76, 77],
-        "g": [55, 57, 59, 60, 62, 64, 66, 67],
-        "a": [57, 59, 61, 62, 64, 66, 68, 69],
-        "b": [59, 61, 63, 64, 66, 68, 70, 71]
-    }
-    
-    #cycle through keys
-    current_key_index = list(key_dictionary).index(current_key)
-
-    try:
-        current_key = list(key_dictionary)[current_key_index + 1]
-
-    except IndexError:
-        current_key = "c"  #reset to "c" if at end
-
-    current_key_scale = key_dictionary[current_key]
-
-
-#generates a new instrument
-def generate_new_instrument():
-
-    global current_instrument
-    instruments = ['Acoustic Grand Piano', 'Rock Organ', 'Music Box', 'Flute', 'Choir Aahs', 'Violin', 'Trumpet', 'Bassoon', 'Cello']
-    try:
-        current_instrument = instruments[(instruments.index(current_instrument) + 1) % len(instruments)]
-    except ValueError:
-        current_instrument = 'Acoustic Grand Piano'
-
-
-#calculates the parameters based on the level the user is on
-def calculate_parameters():
-    
-    global level
-    global current_speed
+@app.route('/get_current_length')
+def get_current_length():
     global current_length
-    global score
+    return jsonify({'current_length': current_length})
 
-    #every 5th level increase speed
-    if level != 0 and level % 5 == 0:
-        current_speed = current_speed - 0.09
-        score +=  2
-    
-    #every 10th level inc length and dec speed by a little (so game isn't impossible)
-    if level != 0 and level % 10 == 0:
-
-        if current_length < 8: #make sure notes don't go out of octive range
-            current_length = current_length + 1
-
-        current_speed = current_speed + 0.02
-        score += 3
-
-    #every 7th level change key
-    if level != 0 and level % 7 == 0:
-        generate_new_key()
-        
-        
-    #every 15th level change instrument
-    if level != 0 and level % 15 == 0:
-        generate_new_instrument()
-
-
-#generates the melody
-@app.route('/generate_melody', methods=['GET'])
-def generate_melody():
-    global random_seq, current_key_scale, current_instrument, current_length, current_speed, score, level, MIDI_FILE_PATH
-    calculate_parameters()
-
-    if current_length > len(current_key_scale):
-        return jsonify({'error': 'Length cannot be greater than the scale size'}), 400
-
-    midi = pretty_midi.PrettyMIDI()
-    sound_program = pretty_midi.instrument_name_to_program(current_instrument)
-    instr = pretty_midi.Instrument(program=sound_program)
-
-    random_seq = random.sample(current_key_scale[:current_length], current_length)
-
-    for i, note_num in enumerate(random_seq):
-        start_time = i * current_speed
-        end_time = (i + 1) * current_speed
-        note = pretty_midi.Note(velocity=100, pitch=note_num, start=start_time, end=end_time)
-        instr.notes.append(note)
-
-    midi.instruments.append(instr)
-    midi.write(MIDI_FILE_PATH)
-
-    level += 1
-    score += 2
-
-    return jsonify({
-        'melody': random_seq,
-        'level': level,
-        'score': score,
-        'instrument': current_instrument,
-        'key': current_key,
-        'speed': current_speed
-    })
-
-#endpoint to play the generated melody
-@app.route('/play_melody', methods=['GET'])
-def play_melody():
-    if os.path.exists(MIDI_FILE_PATH):
-        play_sound(MIDI_FILE_PATH)
-        return jsonify({'status': 'playing'}), 200
-    else:
-        return jsonify({'error': 'No melody file found'}), 400
-
-
-#plays a MIDI file
-def play_sound(midi_file):
-    pygame.mixer.init()
-    pygame.mixer.music.load(midi_file)
-    pygame.mixer.music.play()
-    while pygame.mixer.music.get_busy():
-        time.sleep(1)
+# Endpoint to get current key scale
+@app.route('/get_current_key_scale')
+def get_current_key_scale():
+    global current_key_scale
+    return jsonify({'current_key_scale': current_key_scale})
 
 
 #run the Flask app
--- SoundGame/script.js
+++ SoundGame/script.js
@@ -2,24 +2,11 @@
 //TASK #2: add in the user input part
 
 
-// Fetch and generate the melody
-fetch('http://127.0.0.1:5000/generate_melody')
-    .then(response => response.json())
-    .then(data => {
-        console.log("Generated Melody:", data.melody);
-        console.log("Level:", data.level);
-        console.log("Score:", data.score);
-    })
-    .catch(error => console.error('Error:', error));
+//Fetch and generate the melody
 
-// Play the generated melody
-fetch('http://127.0.0.1:5000/play_melody')
-    .then(response => response.json())
-    .then(data => {
-        console.log(data.status);
-    })
-    .catch(error => console.error('Error:', error));
 
 /**
  * next coding sesh, you need to get thge buttons to show
  */
+
+
