e2cea1668961d5c4af5acdad54f768b920275245
AineHartz <ryanhartz9169@gmail.com>
2024-11-13 13:36:40 -0600

    Revamped all of my rhytym game files. Relaxed and survival modes are properly implemented, and everything should work properly outside of points.

--- Rhythm/app.py
+++ Rhythm/app.py
@@ -1,5 +1,5 @@
 from flask import Flask, jsonify, request
-from flask_cors import  CORS
+from flask_cors import CORS
 import random
 
 app = Flask(__name__)
@@ -7,8 +7,17 @@ CORS(app)
 
 # Endpoint to generate a rhythm
 @app.route('/generate_rhythm', methods=['GET'])
+
+
 def generate_rhythm():
-    length = int(request.args.get('length', 4))  # Default rhythm length of 4 beats
+    """
+    Generates an endless rhythm sequence.
+    
+    Returns:
+        JSON response with progressively faster beats.
+    """
+
+    length = int(request.args.get('length', 4))  # Default rhythm length of 4 beats, but if supplied a length, the default is overridden. 
     rhythm = [random.uniform(0.5, 1.5) for _ in range(length)]  # Random time intervals
     return jsonify({'rhythm': rhythm})
 
